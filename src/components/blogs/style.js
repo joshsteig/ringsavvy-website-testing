677944ab349b808e7a42ea2aff086e4c02acfbca
Oleg Pristashkin <olegserfcompany@gmail.com>
2020-12-07 10:17:06 -0500

    Update src/components/blogs/style.js
    Co-authored-by: answeringLegal <74438930+answeringLegal@users.noreply.github.com>

--- src/components/blogs/style.js
+++ src/components/blogs/style.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
 export const BlogMain = styled.div`
-h2{
-  width:60%;
-}
-@media (max-width: 767px) {
-  grid-template-columns: 1fr;
-  h2{
-    width:100%;
+  h2 {
+    width: 60%;
   }
-}
-`
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+    h2 {
+      width: 100%;
+    }
+  }
+`;
 
 export const BlogsWrapper = styled.div`
   display: grid;

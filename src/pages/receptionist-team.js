c5ad1cffb3e443c12afaed5fb277f82bce8478e0
answeringLegal <74438930+answeringLegal@users.noreply.github.com>
2021-08-04 12:05:38 -0400

    Update receptionist-team.js

--- src/pages/receptionist-team.js
+++ src/pages/receptionist-team.js
@@ -14,23 +14,23 @@ import Footer from '../components/LandingBody/footer';
 export default function RootIndex(props) {
   const { location, data } = props;
 
-  return (
-    <>
+   return (
+    <Layout location={location}>
       <Helmet>
         <title>
-          Best Quality 24/7 Live Answering Service and Virtual Receptionists
+          Answering Service for the Field Service Industry - RingSavvy, Inc.
         </title>
-        <meta
-          name='description'
-          content='Ring Savvy | 24/7 Live Answering Service | Serving All Industries as a Quality Extension of Your Business | Try For Free! Fully Bilingual.'
-        />
       </Helmet>
-      <Navbar />
-      <Benefits />
-      <Persons />
-      <Banner />
-      <Peoples />
-      <Feedback/>
-      <Footer/>
-    </>
+      <Hero heading={heroData.heading} />
+      <BulletSection bulletData={bulletData[0]} />
+      <BulletSection isFlipped bulletData={bulletData[1]} />
+      <BulletSection bulletData={bulletData[2]} />
+      <CardGrid
+        contentData={contentData.find((data) => data.id === 2)}
+        cardData={cardData}
+      />
+      <Testimonials />
+      <FaqAccordion contentData={contentData.find((data) => data.id === 1)} />
+    </Layout>
   );
+}

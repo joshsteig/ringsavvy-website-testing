335cf91894c770054394ddd27d1284e7be208967
answeringLegal <74438930+answeringLegal@users.noreply.github.com>
2021-08-11 14:46:41 -0400

    Update index.js

--- src/components/landingHeader/index.js
+++ src/components/landingHeader/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Logo from '../logo';
-import { Link } from 'react-router';
 
 const index = () => {
   return (

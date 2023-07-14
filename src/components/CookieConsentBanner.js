38b0c8efacf69eeadd1628272ba7ec93c52f38c8
joshsteig <130782021+joshsteig@users.noreply.github.com>
2023-07-14 17:07:28 -0400

    Updated cookie consent banner

--- src/components/CookieConsentBanner.js
+++ src/components/CookieConsentBanner.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CookieConsent from 'react-cookie-consent';
+import Cookies from 'js-cookie';
+
 const CookieConsentBanner = () => {
+  const [cookiesEnabled, setCookiesEnabled] = useState(Cookies.get('cookiesEnabled'));
+
+  const handleToggleCookies = () => {
+    const enabled = !cookiesEnabled;
+    setCookiesEnabled(enabled);
+  
+    if (enabled) {
+      // User has enabled cookies
+      Cookies.set('cookiesEnabled', 'true');
+    } else {
+      // User has disabled cookies
+      Cookies.remove('cookiesEnabled');
+  
+      // Remove any existing cookies
+      const allCookies = Cookies.get();
+      Object.keys(allCookies).forEach((cookieName) => {
+        Cookies.remove(cookieName, { sameSite: 'Strict' });
+      });
+    }
+  };
+
   return (
     <CookieConsent
-      location='bottom'
-      buttonText='Accept'
-      declineButtonText='Decline'
-      cookieName='cookieConsent'
-      enableDeclineButton
+      location="bottom"
+      buttonText={cookiesEnabled ? 'Disable Cookies' : 'Enable Cookies'}
+      onAccept={handleToggleCookies}
     >
-      {' '}
-      This website uses cookies to enhance the user experience. Accept or
-      decline the use of cookies.{' '}
+      This website uses cookies to ensure you get the best experience.{' '}
+      {cookiesEnabled ? 'Cookies are enabled.' : 'Cookies are disabled.'}
     </CookieConsent>
   );
 };
+
 export default CookieConsentBanner;
